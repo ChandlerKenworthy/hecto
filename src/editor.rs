ef7a41dafab49d195d5dbacd1ab4a2b7af8a3794
Chandler Kenworthy <chandlerkenworthy@gmail.com>
2026-02-22 22:14:55 +0000

    Refactor

--- src/editor.rs
+++ src/editor.rs
@@ -1,5 +1,7 @@
-use crossterm::event::{read, Event::Key, KeyCode::Char, KeyEvent, KeyModifiers};
-use crossterm::terminal::{enable_raw_mode, disable_raw_mode};
+use crossterm::event::{read, Event, Event::Key, KeyCode::Char, KeyEvent, KeyModifiers};
+use crossterm::terminal::{enable_raw_mode, disable_raw_mode, Clear, ClearType};
+use crossterm::execute;
+use std::io::stdout;
 
 pub struct Editor {
     should_quit: bool
@@ -11,34 +13,57 @@ impl Editor {
     }
 
     pub fn run(&mut self) {
-        if let Err(err) = self.repl() {
-            panic!("{err:#?}");
+        Self::initialize().unwrap();
+        let result = self.repl();
+        Self::terminate().unwrap();
+        result.unwrap();
+    }
+
+    fn initialize() -> Result<(), std::io::Error> {
+        enable_raw_mode()?;
+        Self::clear_screen()
+    }
+
+    fn clear_screen() -> Result<(), std::io::Error> {
+        let mut stdout = stdout();
+        execute!(stdout, Clear(ClearType::All))
+    }
+
+    fn terminate() -> Result<(), std::io::Error> {
+        disable_raw_mode()
+    }
+
+    fn refresh_screen(&self) -> Result<(), std::io::Error> {
+        if self.should_quit {
+            Self::clear_screen()?;
+            print!("Goodbye.\r\n");
         }
-        println!("Goodbye.\r\n");
+        Ok(())
     }
 
     fn repl(&mut self) -> Result<(), std::io::Error> {
-        enable_raw_mode()?;
-
         loop {
-            if let Key(KeyEvent {
-                code, modifiers, kind, state
-            }) = read()? 
-            {
-                println!("Code: {code:?} Modifiers: {modifiers:?} Kind: {kind:?} State: {state:?} \r");
-                match code {
-                    Char('q') if modifiers == KeyModifiers::CONTROL => {
-                        self.should_quit = true;
-                    },
-                    _ => ()
-                }
-            }
-            if self.should_quit == true {
+            let event = read()?;
+            self.evaluate_event(&event);
+            self.refresh_screen()?;
+            if self.should_quit {
                 break;
             }
         }
-
-        disable_raw_mode()?;
         Ok(())
     }
+
+    fn evaluate_event(&mut self, event: &Event) {
+        if let Key(KeyEvent {
+            code, modifiers, ..
+        }) = event {
+            match code {
+                Char('q') if *modifiers == KeyModifiers::CONTROL => {
+                    self.should_quit = true;
+                },
+                _ => (),
+            }
+        }
+    }
+
 }
